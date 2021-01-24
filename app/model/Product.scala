d80246619af6eda1265648b77b2aa9be3b00a2a3
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2021-01-24 14:52:11 +0000

    [SA] created a Item model and connect app to db

--- app/model/Product.scala
+++
@@ -1,9 +0,0 @@
-package model
-
-final case class Id      (id: String)               extends AnyVal
-final case class Name    (name: String)             extends AnyVal
-final case class Price   (price: Double)            extends AnyVal
-final case class Discount(discount: Option[Double]) extends AnyVal
-final case class Stock   (stock: Int)               extends AnyVal
-
-case class Product(id: Id, name: Name, price: Price, discount: Discount, stock: Stock)
